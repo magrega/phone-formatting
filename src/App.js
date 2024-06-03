cd6a54fe6a3f71a407e42f624b6e6c22956b9b7b
bobmagrega <proshanov@yandex.ru>
2024-06-03 12:13:32 +0300

    refactored code

--- src/App.js
+++ src/App.js
@@ -3,23 +3,20 @@ import './App.css';
 
 const App = () => {
   const [text, setText] = useState("");
-  
-  const handleInputChange = (e) => {
-    let formatInput = formatPhoneNumber(e.target.value)
-    setText(String(formatInput));
-  }
 
-  const formatPhoneNumber = (number) => {
-    let text = ("" + number).replace(/\D/g, "");
-    let match = text.match(/^(\d{0,3})?(\d{0,3})?(\d{0,4})?/);
-    const result = [match[1] ? "(" : "",
-    match[1],
-    match[2] ? ") " : "",
-    match[2],
-    match[3] ? "-" : "",
-    match[3]].join("");
+  const handleInputChange = e => setText(formatPhoneNumber(e.target.value));
 
-    return result;
+  const formatPhoneNumber = (phoneNumber) => {
+    const onlyNumbers = (phoneNumber).replace(/\D/g, "");
+    const matches = onlyNumbers.match(/^(\d{0,3})?(\d{0,3})?(\d{0,4})?/);
+    const formatedPhoneNumber = [matches[1] ? "(" : "",
+    matches[1],
+    matches[2] ? ") " : "",
+    matches[2],
+    matches[3] ? "-" : "",
+    matches[3]].join("");
+
+    return formatedPhoneNumber;
   }
 
   return (
