1fc0287d680bc7ecec278a11d6c4847f36587501
bobmagrega <proshanov@yandex.ru>
2024-06-03 12:08:41 +0300

    made a simple phone formatting input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [text, setText] = useState("");
+  
+  const handleInputChange = (e) => {
+    let formatInput = formatPhoneNumber(e.target.value)
+    setText(String(formatInput));
+  }
+
+  const formatPhoneNumber = (number) => {
+    let text = ("" + number).replace(/\D/g, "");
+    let match = text.match(/^(\d{0,3})?(\d{0,3})?(\d{0,4})?/);
+    const result = [match[1] ? "(" : "",
+    match[1],
+    match[2] ? ") " : "",
+    match[2],
+    match[3] ? "-" : "",
+    match[3]].join("");
+
+    return result;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Phone number formatting</h1>
+      <span>Enter phone number: </span><input type='text' placeholder='(XXX)XXX-XXXX' onChange={handleInputChange} value={text} />
     </div>
   );
 }
